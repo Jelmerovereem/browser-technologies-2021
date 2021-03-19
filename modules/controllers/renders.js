6f6d6c0465bf1c89e61fb40044ca5547b84a7c25
Jelmer Overeem <jelmer_overeem@hotmail.com>
2021-03-19 14:33:56 +0100

    added renders

---
+++ modules/controllers/renders.js
@@ -0,0 +1,84 @@
+import { db } from "../../server.js";
+
+function renderAdminLogin(req, res) {
+	if (!req.session.admin) {
+		res.render("adminLogin.ejs");
+	} else {
+		res.redirect("/dashboard");
+	}
+}
+
+async function renderAdminDashboard(req, res) {
+	if (!req.session.admin) {
+		res.redirect("/admin")
+	} else {
+		const matches = await db.collection("matches").find().toArray();
+		const adminData = req.session.admin;
+		res.render("adminDashboard.ejs", {
+			adminData,
+			matches
+		});
+	}
+}
+
+function renderAddmatch(req, res) {
+	if (!req.session.admin) {
+		res.redirect("/admin")
+	} else {
+		const adminData = req.session.admin;
+		res.render("addMatch.ejs", {
+			adminData
+		});
+	}
+}
+
+async function renderEditMatch(req, res) {
+	if (!req.session.admin) {
+		res.redirect("/admin")
+	} else {
+		const matchId = req.params.matchId;
+		const match = await db.collection("matches").findOne({id: matchId});
+		console.log(match);
+		res.render("editMatch.ejs", {
+			match
+		})
+	}
+}
+
+function adminSignout(req, res) {
+	req.session.admin = "";
+	res.redirect("/admin");
+}
+
+async function renderUserHome(req, res) {
+	const matches = await db.collection("matches").find().toArray();
+	res.render("userHome.ejs", {
+		matches
+	})
+}
+
+async function renderUserMatch(req, res) {
+	const matchId = req.params.matchId;
+	const match = await db.collection("matches").findOne({id: matchId});
+	res.render("detailMatch.ejs", {
+		match
+	})
+}
+
+async function renderProfile(req, res) {
+	if (!req.session.user) {
+		res.render("userLogin.ejs");
+	} else {
+		const profile = req.session.user;
+		res.render("profile.ejs", {
+			profile
+		})
+	}
+}
+
+function userSignout(req, res) {
+	req.session.user = "";
+	res.redirect("/profile");
+}
+
+export { renderAdminLogin, renderAdminDashboard, adminSignout, renderAddmatch, renderUserHome, renderEditMatch, renderUserMatch, renderProfile, userSignout }
